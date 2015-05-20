4580f928d1941c6ce72c288ace46a70d69b0a125
OndrejM <ondrej.mihalyi@gmail.com>
2015-05-21 00:40:13 +0200

    Pridanie dokumentacie a komentarov

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Vertx3GradleDemo
 Demo of a Vertx 3 application with Gradle build
+
+## Development in Netbeans
+
+Prerequisites: Netbeans Gradle support. Git support recommended.
+
+1. Import as Gradle project
+2. Build, Run and Debug work from context menu on the project
+3. Execute Run/Debug. When code is changed, do Build and code is redeployed (because of Vertx auto-redeploy option)
+4. Optionally in Netbeans options, mark compile on save in Gradle plugin -> Other
+
+## Build and execute JAR
+
+The build.gradle uses the Gradle shadowJar plugin to assemble the application and all it’s dependencies into a single "fat" jar.
+
+To build the "fat jar"
+
+    ./gradlew shadowJar
+
+To run the fat jar:
+
+    java -jar build/libs/Vertx3GradleDemo-3.0.0-SNAPSHOT-fat.jar
+
+## Application documentation
+
+After executio, point browser to http://localhost:8080
--- src/main/java/io/vertx/example/AppLauncher.java
+++ src/main/java/io/vertx/example/AppLauncher.java
@@ -6,7 +6,6 @@
 package io.vertx.example;
 
 import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Starter;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 
@@ -16,11 +15,27 @@ import io.vertx.core.VertxOptions;
  */
 public class AppLauncher {
     public static void main(String[] args) {
-        //Starter.main(new String[] { "run", HelloWorldVerticle.class.getName()});
-        Vertx vertx = Vertx.vertx();
+        /* vertxOptions configure how vertx is executed - cluster, number of threads in pool, etc.
+           Can be configured using JSON - best pattern is to pass JSON configuration/file as a parameter/property
+        */
+        final VertxOptions vertxOptions = new VertxOptions();
+        
+        // create core vertx framework instance (container)
+        Vertx vertx = Vertx.vertx(vertxOptions);
+
+        // deploy verticles, usually loader verticles (modules) that load other verticles
+        deployVerticles(vertx);
+        
+    }
+
+    private static void deployVerticles(Vertx vertx) {
+        /* deployment options - should be tuned for environment
+           E.g. in development, redeploy is true
+           Can be configured using JSON - best pattern is to pass JSON configuration/file as a parameter/property
+        */
         final DeploymentOptions deploymentOptions = new DeploymentOptions();
         deploymentOptions.setRedeploy(true);
-        vertx.deployVerticle(HelloWorldVerticle.class.getName(), deploymentOptions);
         
+        vertx.deployVerticle(HelloWorldVerticle.class.getName(), deploymentOptions);
     }
 }
